02123891e41c1c074e3bd73561c8d6a66fa07827
natanaulrich <natanaulrich@gmail.com>
2026-02-24 22:37:23 -0300

    Add files via upload

---
+++ aula2402exercicio.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 24/02 Exercicio</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            width: 100%;
+            height: 80px;
+            background: pink;
+        }
+
+        #inicio {
+            height: 400px;
+            background: green;
+        }
+    </style>
+</head>
+<body>
+    <header>
+
+    </header>
+
+    <section id="inicio">
+
+    </section>
+</body>
+</html>
